94be1cd4c312f49d7471ef4da56ed4837969d373
CVN32 <cv.cv.cv2nv@gmail.com>
2026-09-18 04:17:53 +0900

    fix(build): make MoneroOcean source fetch reproducible

--- xmrig/lib-builder/script/xmrig-mo-fetch.sh
+++ xmrig/lib-builder/script/xmrig-mo-fetch.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 source script/env.sh
 
-cd $EXTERNAL_LIBS_BUILD_ROOT
-
+cd "$EXTERNAL_LIBS_BUILD_ROOT"
 version="v6.16.5-mo1"
 
-if [ ! -d "xmrig-mo" ]; then
-  git clone https://github.com/MoneroOcean/xmrig.git -b ${version} xmrig-mo
+if [ ! -d xmrig-mo/.git ]; then
+  rm -rf xmrig-mo
+  git clone --depth 1 --branch "$version" https://github.com/MoneroOcean/xmrig.git xmrig-mo
 else
-  cd xmrig-mo
-  git checkout ${version}
+  git -C xmrig-mo fetch --depth 1 origin "refs/tags/$version:refs/tags/$version" || true
+  git -C xmrig-mo reset --hard
+  git -C xmrig-mo clean -fdx
+  git -C xmrig-mo checkout --force "$version"
 fi
